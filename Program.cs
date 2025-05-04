3a88aaf14f77ac21940a0b4b3bdc2d1d9b29c0fa
RKafka <rkafka14@gmail.com>
2025-05-03 19:04:21 -0500

    Exercise - Complete a challenge activity to discover and implement a method call

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Time to get familiar with C#");
+
+Random dice = new Random(); // Random dice = new();
+int roll = dice.Next(1, 7);
+Console.WriteLine("You rolled a " + roll);
+Console.WriteLine(Math.Max(500, 600));
