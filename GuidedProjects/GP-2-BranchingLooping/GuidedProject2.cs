a70b3373e0bc3e9c6c653eb6f70bf816b80852bb
RKafka <rkafka14@gmail.com>
2025-05-07 06:29:22 -0500

    GP2 - added completion message to case 4

--- GuidedProjects/GP-2-BranchingLooping/GuidedProject2.cs
+++ GuidedProjects/GP-2-BranchingLooping/GuidedProject2.cs
@@ -370,6 +370,7 @@ do
                 }
             }
 
+            Console.WriteLine("Nickname & personality description fields are complete for all of our friends.");
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
