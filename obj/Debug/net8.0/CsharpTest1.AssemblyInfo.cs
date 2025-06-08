a4009e8ac7672bacfdc9f76e55199c3e5ecfd6f9
RKafka <rkafka14@gmail.com>
2025-06-08 08:38:43 -0500

    added new .gitignore

--- obj/Debug/net8.0/CsharpTest1.AssemblyInfo.cs
+++ obj/Debug/net8.0/CsharpTest1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsharpTest1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+143b6bb923a305ffea0015c16aed5ed671a7c93c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+656b48539bf605e9df9c3bab2ccd513d3097eb8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsharpTest1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsharpTest1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
